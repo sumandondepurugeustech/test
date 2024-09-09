4b9e394b4f13b6736c1c3803cdc00c7347ead0d6
sumandondepurugeustech <suman.d@ustechsolutionsinc.com>
2024-09-09 14:39:05 +0530

    First Commit

---
+++ src/main/java/practice/automation/Test.java
@@ -0,0 +1,18 @@
+package practice.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com");
+	driver.close();
+	}
+
+}
